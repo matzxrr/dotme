d3bc38d89da99ebf79852c8ad36f9b1111b5c779
matzxrr <matzxrr@users.noreply.github.com>
2023-12-21 12:17:35 -0700

    feat: add crate anyhow and whichrs, setup init to make sure git exists

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{anyhow, Result};
+use clap::{Parser, Subcommand};
+use which::which;
+
+fn git_exists() -> Result<bool> {
+    let result = which("git");
+    if result.is_err() {
+        return Err(anyhow!(
+            "Cound not find 'git' on your PATH. Make sure it is installed."
+        ));
+    }
+    Ok(true)
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+}
+
+fn main() -> Result<()> {
+    git_exists()?;
+
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Init) => {
+            println!("Calling init");
+        }
+        None => {}
+    }
+
+    Ok(())
 }
