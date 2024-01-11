04db82cd107e639d31cd29f6455dd08b5aec8e65
matzxrr <matzxrr@users.noreply.github.com>
2024-01-10 23:52:27 -0700

    feat: working on testing library that creates a bare repo and detroys it when the repo instance goes out of scope

--- dotme-test/src/lib.rs
+++ dotme-test/src/lib.rs
@@ -1,15 +1,37 @@
-use directories::BaseDirs;
+use std::{env, fs, path::Path};
+
 use git2::Repository;
 
-pub fn create_temp_repo() -> Repository {
-    let base_dirs = match BaseDirs::new() {
-        Some(p) => p,
-        None => {
-            panic!("couldin't find base_dir");
-        }
-    };
-    match Repository::init(base_dirs.home_dir().join(".tmp")) {
-        Ok(repo) => repo,
+pub struct TestRepo(Repository);
+
+impl Drop for TestRepo {
+    fn drop(&mut self) {
+        let repo_path = self.0.path();
+        println!("Destorying {}", repo_path.display());
+        // if let Err(err) = std::fs::remove_dir_all(repo_path) {
+        //     eprintln!("Unable to destory {} - {}", repo_path.display(), err);
+        // };
+    }
+}
+
+/// Creates a test repository that gets destroyed on `Drop`
+pub fn create_temp_repo() -> TestRepo {
+    let temp_dir = Path::new(".tmp");
+    println!("creating dir {}", temp_dir.display());
+    if let Err(err) = fs::create_dir_all(temp_dir) {
+        panic!(
+            "Unable to create temp directory {} - {}",
+            temp_dir.display(),
+            err
+        );
+    }
+    let name = std::thread::current()
+        .name()
+        .expect("Can't get thread name")
+        .to_owned();
+    let temp_git_dir = temp_dir.join(name);
+    match Repository::init_bare(temp_git_dir) {
+        Ok(repo) => TestRepo(repo),
         Err(e) => panic!("failed to create temp repo: {}", e),
     }
 }
