514c7d1527ccafe4fe21f27baf356d63c3f6d21e
matzxrr <matzxrr@users.noreply.github.com>
2024-01-29 23:37:33 -0700

    feat: added better tests to file shell.rs

--- dotme-core/src/lib.rs
+++ dotme-core/src/lib.rs
@@ -2,7 +2,6 @@
 
 /// Core functionality of the cli
 pub mod cmd;
-pub mod config;
 pub mod path_utils;
 pub mod repo;
 pub mod shell;
--- dotme-core/src/shell.rs
+++ dotme-core/src/shell.rs
@@ -1,7 +1,7 @@
 use std::env;
 use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, PartialEq)]
 pub enum ShellError {
     #[error("Can't find the users shell, might not be supported")]
     CannotFindShell,
@@ -9,7 +9,7 @@ pub enum ShellError {
 
 type Result<T> = std::result::Result<T, ShellError>;
 
-#[derive(Default)]
+#[derive(Default, Debug, PartialEq)]
 pub struct ShellConfig {
     pub shell: String,
     pub path: String,
@@ -22,20 +22,26 @@ impl ShellConfig {
     ///
     /// TODO: Need to find fallback ways of checking a users shell
     pub fn load() -> Result<ShellConfig> {
-        let shell_result = env::var("SHELL");
-        if let Ok(shell_path) = shell_result {
-            if let Some(shell_name) = get_shell_name_from_path(&shell_path) {
-                if let Some(shell_file) = match_shell_to_config_file(&shell_name) {
-                    return Ok(ShellConfig {
-                        shell: shell_name,
-                        path: shell_path,
-                        file: shell_file,
-                    });
-                }
+        if let Ok(shell_path) = env::var("SHELL") {
+            if let Ok(shell_config) = try_parse_shell(&shell_path) {
+                return Ok(shell_config);
             }
         }
         Err(ShellError::CannotFindShell)
     }
+}
+
+fn try_parse_shell(shell_path: &str) -> Result<ShellConfig> {
+    if let Some(shell_name) = get_shell_name_from_path(shell_path) {
+        if let Some(shell_file) = match_shell_to_config_file(&shell_name) {
+            return Ok(ShellConfig {
+                shell: shell_name,
+                path: shell_path.to_owned(),
+                file: shell_file,
+            });
+        }
+    }
+    Err(ShellError::CannotFindShell)
 }
 
 fn match_shell_to_config_file(shell: &str) -> Option<String> {
@@ -58,23 +64,27 @@ fn get_shell_name_from_path(shell: &str) -> Option<String> {
 }
 
 #[cfg(test)]
-mod shell_tests {
+mod try_parse_shell_tests {
     use super::*;
 
     #[test]
-    fn it_should_load_users_shell() {
-        let shell = ShellConfig::load().expect("load shell");
+    fn it_should_load_bash_shell() {
+        let shell = try_parse_shell("/bin/bash").expect("'/bin/bash' should parse");
         assert_eq!(&shell.shell, "bash");
         assert_eq!(&shell.file, ".bashrc");
         assert_eq!(&shell.path, "/bin/bash");
     }
 
     #[test]
-    fn test_something() {
-        if let Ok(exe_path) = env::current_exe() {
-            if let Some(parent) = exe_path.parent() {
-                println!("{}", parent.display());
-            }
-        }
+    fn it_should_load_zsh_shell() {
+        let shell = try_parse_shell("/usr/bin/zsh").expect("'/usr/bin/zsh' should parse");
+        assert_eq!(&shell.shell, "zsh");
+        assert_eq!(&shell.file, ".zshrc");
+        assert_eq!(&shell.path, "/usr/bin/zsh");
+    }
+    #[test]
+    fn it_shouldnt_load_unknown_shell() {
+        let shell = try_parse_shell("/usr/bin/unknown");
+        assert_eq!(shell, Err(ShellError::CannotFindShell));
     }
 }
