7726925a598a54ea458e3d6a057f041f1a1c3066
matzxrr <matzxrr@gmail.com>
2024-02-26 22:55:58 -0700

    feat: cleanup clone and start new crate for path utils

--- dotme-core/src/cmd/clone.rs
+++ dotme-core/src/cmd/clone.rs
@@ -1,15 +1,9 @@
 use std::path::{Path, PathBuf};
 
 use anyhow::Result;
-use dialoguer::{theme::ColorfulTheme, Input, Password};
-use git2::Cred;
+use dialoguer::{theme::ColorfulTheme, Input};
 
-use crate::{
-    path_utils::{is_ssh_path, parse_into_absolute},
-    repo::Repo,
-};
-
-pub fn clone(remote: &Path) -> Result<()> {
+pub fn clone(_remote: &Path) -> Result<()> {
     println!("\n\nCloning a repository");
 
     let default_path = String::from("$HOME/.cfg");
@@ -27,30 +21,7 @@ pub fn clone(remote: &Path) -> Result<()> {
         })
         .interact_text()?;
 
-    let repo_path = PathBuf::from(repo_location);
-    let absolute_repo_path = parse_into_absolute(&repo_path);
-    let is_remote_ssh = is_ssh_path(remote);
-
-    let cred;
-    if is_remote_ssh {
-        let default_ssh_key = String::from("~/.ssh/id_rsa");
-        let ssh_key = Input::with_theme(&ColorfulTheme::default())
-            .with_prompt("SSH Key")
-            .default(default_ssh_key)
-            .interact_text()?;
-        let ssh_password = Password::with_theme(&ColorfulTheme::default())
-            .with_prompt("SSH Password")
-            .interact();
-        cred = Cred::ssh_key(
-            username_from_url.unwrap(),
-            None,
-            Path::new(&format!("{}/.ssh/id_rsa", env::var("HOME").unwrap())),
-            None,
-        )?;
-    }
-
-    // let _result = Repo::clone(remote, &absolute_repo_path)?;
-    // println!("Found {}", remote);
+    dbg!(repo_location);
 
     Ok(())
 }
