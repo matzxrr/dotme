0b10df3a38908e324e8de56d3ef713682ef5abf8
matzxrr <matzxrr@gmail.com>
2024-10-27 19:15:05 -0600

    feat: stabalize code back to something that works

--- dotme-path/src/interpolate_env/tests.rs
+++
@@ -1,9 +0,0 @@
-use super::*;
-
-#[test]
-fn it_should_parse_basic_env() {
-    let input = Path::new("someting/test/$CARGO_PKG_NAME");
-    let result = interpoate_env(input).expect("Should parse env variable");
-    let expected = PathBuf::from("something/test/dotme-path");
-    assert_eq!(expected, result);
-}
--- dotme-path/src/lib.rs
+++ dotme-path/src/lib.rs
@@ -1,8 +1,1 @@
 //! Path utilities for dotme
-//!
-//! Generally doesn't support Windows
-
-/// Interpolate Env
-///
-/// Bash like interpolation of ENV variables in `Path` structs.
-pub mod interpolate_env;
